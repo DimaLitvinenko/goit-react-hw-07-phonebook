5fcf200489dd1eccfda8535ff35235ed490640e3
DimaLitvinenko <dimaly.too@gmail.com>
2021-12-14 14:21:29 +0200

    added localstorage

--- src/App.js
+++ src/App.js
@@ -6,89 +6,81 @@ import Section from './components/Section/Section';
 import ContactsList from './components/ContactsList/ContactsList';
 import ContactsForm from './components/ContactsForm/ContactsForm';
 import Filter from './components/Filter/Filter';
-import earlyContacts from '../src/data/contacts.json';
+import earlyAddContacts from '../src/data/contacts.json';
 
 export default class App extends Component {
-	state = {
-		contacts: earlyContacts,
-		filter: '',
-	};
+   static defaultProps = {
+      contacts: earlyAddContacts,
+      filter: '',
+   };
 
-	addContact = ({ name, number }) => {
-		this.state.contacts.some(contact => name === contact.name)
-			? alert(`${name} is already in contacts`)
-			: this.setState(({ contacts }) => ({
-					contacts: [...contacts, { name, number, id: nanoid() }],
-			  }));
-	};
+   state = {
+      contacts: this.props.contacts,
+      filter: this.props.filter,
+   };
 
-	deleteContact = contactId => {
-		this.setState(prevState => ({
-			contacts: prevState.contacts.filter(
-				contact => contact.id !== contactId,
-			),
-		}));
-	};
+   componentDidMount() {
+      const contacts = JSON.parse(localStorage.getItem('contacts'));
+      contacts && this.setState({ contacts });
+   }
 
-	findByName = ({ currentTarget }) => {
-		this.setState({
-			filter: currentTarget.value,
-		});
-	};
+   componentDidUpdate(prevState) {
+      if (this.state.contacts !== prevState.contacts) {
+         localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+      }
+   }
 
-	filterList = () => {
-		const { contacts, filter } = this.state;
-		const filteredItems = filter.toLowerCase();
+   addContact = ({ name, number }) => {
+      this.state.contacts.some(contact => name === contact.name)
+         ? alert(`${name} is already in contacts`)
+         : this.setState(({ contacts }) => ({
+              contacts: [...contacts, { name, number, id: nanoid() }],
+           }));
+   };
 
-		return contacts.filter(contact =>
-			contact.name.toLowerCase().includes(filteredItems),
-		);
-	};
+   deleteContact = contactId => {
+      this.setState(prevState => ({
+         contacts: prevState.contacts.filter(
+            contact => contact.id !== contactId,
+         ),
+      }));
+   };
 
-	render() {
-		const { filter } = this.state;
-		const { addContact, findByName, filterList, deleteContact } = this;
-		return (
-			<main className="App">
-				<Container>
-					<Section>
-						<h1 className="app-title">Phonebook</h1>
-						<ContactsForm addContactHandler={addContact} />
-					</Section>
-					<Section>
-						<h2 className="app-title">Contacts</h2>
-						<Filter contactName={filter} findByNameHandler={findByName} />
-						<ContactsList
-							contacts={filterList()}
-							deleteContactHandler={deleteContact}
-						/>
-					</Section>
-				</Container>
-			</main>
-		);
-	}
+   findByName = ({ currentTarget }) => {
+      this.setState({
+         filter: currentTarget.value,
+      });
+   };
+
+   filterList = () => {
+      const { contacts, filter } = this.state;
+      const filteredItems = filter.toLowerCase();
+
+      return contacts.filter(contact =>
+         contact.name.toLowerCase().includes(filteredItems),
+      );
+   };
+
+   render() {
+      const { filter } = this.state;
+      const { addContact, findByName, filterList, deleteContact } = this;
+      return (
+         <main className="App">
+            <Container>
+               <Section>
+                  <h1 className="app-title">Phonebook</h1>
+                  <ContactsForm addContactHandler={addContact} />
+               </Section>
+               <Section>
+                  <h2 className="app-title">Contacts</h2>
+                  <Filter contactName={filter} findByNameHandler={findByName} />
+                  <ContactsList
+                     contacts={filterList()}
+                     deleteContactHandler={deleteContact}
+                  />
+               </Section>
+            </Container>
+         </main>
+      );
+   }
 }
-
-// ===================
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
