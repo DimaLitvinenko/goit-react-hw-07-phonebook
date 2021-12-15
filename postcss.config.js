7520872e57d02fb0820cd4d2f14eb4264151b243
DimaLitvinenko <dimaly.too@gmail.com>
2021-12-15 19:01:12 +0200

    update

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	// You can specify any options from https://postcss.org/api/#processoptions here
-	// parser: 'sugarss',
-	plugins: [
-		// Plugins for PostCSS
-		['postcss-short', { prefix: 'x' }],
-		'postcss-preset-env',
-	],
-};
